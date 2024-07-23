2dae9bd5bbc6ac276254b46f64c0d20f50c77e36
Isak Ellmer <isak01@gmail.com>
2024-07-23 21:43:15 +0200

    Make time constants long long

--- include/timeman.h
+++ include/timeman.h
@@ -31,8 +31,8 @@ extern volatile atomic_int uciponder;
 /* timepoint_t is given in nanoseconds. */
 typedef int64_t timepoint_t;
 
-#define TPPERSEC 1000000000l
-#define TPPERMS     1000000l
+#define TPPERSEC 1000000000ll
+#define TPPERMS     1000000ll
 
 struct timeinfo {
 	int stop_on_time;
