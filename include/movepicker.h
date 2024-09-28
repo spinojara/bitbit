7cdf2defcc3ce7e19eaa0a63c01727b826ee3795
Isak Ellmer <isak01@gmail.com>
2024-09-28 15:39:45 +0200

    Export sort_moves

--- include/movepicker.h
+++ include/movepicker.h
@@ -59,4 +59,6 @@ move_t next_move(struct movepicker *mp);
 
 void movepicker_init(struct movepicker *mp, int quiescence, struct position *pos, const struct pstate *pstate, move_t ttmove, move_t killer1, move_t killer2, move_t counter_move, const struct searchinfo *si);
 
+void sort_moves(struct movepicker *mp);
+
 #endif
